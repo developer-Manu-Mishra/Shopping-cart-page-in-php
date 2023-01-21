0b5ace60ea72f58ca5ee00faddff9dabfd6e4017
developer-Manu-Mishra <122797473+developer-Manu-Mishra@users.noreply.github.com>
2023-01-21 23:42:48 +0530

    All files of carts
    All files of carts

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+	$conn = new mysqli("localhost","id20183132_cart_system01","6BUmcB*!r>H4RYeo","id20183132_cart_system");
+	if($conn->connect_error){
+		die("Connection Failed!".$conn->connect_error);
+	}
+?>
